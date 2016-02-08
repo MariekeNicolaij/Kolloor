3c93cf62f4023bf64b86420fb15a4de3f6353500
Henky95 <henkkakkerlak@hotmail.com>
2016-02-08 14:44:16 +0100

    cleaned up some scripts

---
+++ Project/Kolloor/Assets/Resources/Scripts/AI/States/HelpState.cs
@@ -0,0 +1,12 @@
+﻿using UnityEngine;
+using System.Collections;
+
+namespace AI.States
+{
+    public class HelpState : State
+    {
+        public override void Enter() { }
+        public override void Execute() { }
+        public override void Exit() { }
+    }
+}
---
+++ Project/Kolloor/Assets/Resources/Scripts/AI/States/IdleState.cs
@@ -0,0 +1,12 @@
+﻿using UnityEngine;
+using System.Collections;
+
+namespace AI.States
+{
+    public class IdleState : State
+    {
+        public override void Enter() { }
+        public override void Execute() { }
+        public override void Exit() { }
+    }
+}
